d6e6fb5a3d2660a2d5093341eadc9f61ca8c94a7
jay <jay.wekeep@gmail.com>
2022-09-14 08:26:03 +0900

    Mustache 해보기

--- src/main/java/com/jay/getinline/config/ThymeleafConfig.java
+++ src/main/java/com/jay/getinline/config/ThymeleafConfig.java
@@ -1,12 +1,6 @@
 package com.jay.getinline.config;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 /**
  * application.yml 을 통해 properties 설정을 추가하기 위한 configuration
@@ -14,7 +8,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 @Configuration
 public class ThymeleafConfig {
 
-    @Bean
+    /*@Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
             SpringResourceTemplateResolver defaultTemplateResolver,
             Thymeleaf3Properties thymeleaf3Properties
@@ -29,9 +23,9 @@ public class ThymeleafConfig {
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
 
-        /**
-         * Thymeleaf 3 Decoupled logic 기능 활성화
-         */
+        *//**
+     * Thymeleaf 3 Decoupled logic 기능 활성화
+     *//*
         private final boolean decoupledLogic;
-    }
+    }*/
 }
