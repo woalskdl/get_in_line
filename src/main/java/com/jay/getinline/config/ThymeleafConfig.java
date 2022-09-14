ff815bf50594a23535d4b39b9bdd81e41c61c638
jay <jay.wekeep@gmail.com>
2022-09-14 09:07:12 +0900

    Thymeleaf 로 돌아와서 강의 진행

--- src/main/java/com/jay/getinline/config/ThymeleafConfig.java
+++ src/main/java/com/jay/getinline/config/ThymeleafConfig.java
@@ -1,6 +1,12 @@
 package com.jay.getinline.config;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 /**
  * application.yml 을 통해 properties 설정을 추가하기 위한 configuration
@@ -8,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ThymeleafConfig {
 
-    /*@Bean
+    @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
             SpringResourceTemplateResolver defaultTemplateResolver,
             Thymeleaf3Properties thymeleaf3Properties
@@ -22,10 +28,7 @@ public class ThymeleafConfig {
     @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
-
-        *//**
-     * Thymeleaf 3 Decoupled logic 기능 활성화
-     *//*
+//        Thymeleaf 3 Decoupled logic 기능 활성화
         private final boolean decoupledLogic;
-    }*/
+    }
 }
