f0a7dcabc5d7f8d63f932db3319389b12fe45d64
jay <jay.wekeep@gmail.com>
2022-10-05 09:23:55 +0900

    devtools 추가 테스트 및 누락코드 추가

--- src/main/java/com/jay/getinline/controller/EventController.java
+++ src/main/java/com/jay/getinline/controller/EventController.java
@@ -1,49 +1,39 @@
 package com.jay.getinline.controller;
 
-import com.jay.getinline.constant.EventStatus;
+import com.jay.getinline.constant.ErrorCode;
+import com.jay.getinline.domain.Event;
 import com.jay.getinline.dto.EventResponse;
+import com.jay.getinline.exception.GeneralException;
+import com.jay.getinline.service.EventService;
+import com.querydsl.core.types.Predicate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/events")
 @Controller
+@RequiredArgsConstructor
 public class EventController {
 
+    private final EventService eventService;
+
     @GetMapping
-    public ModelAndView events() {
-
+    public ModelAndView events(@QuerydslPredicate(root = Event.class) Predicate predicate) {
         Map<String, Object> map = new HashMap<>();
-        map.put("events", List.of(EventResponse.of(
-                        1L,
-                        1L,
-                        "오후 운동",
-                        EventStatus.OPENED,
-                        LocalDateTime.of(2022, 8, 31, 9, 0, 0),
-                        LocalDateTime.of(2022, 8, 31, 18, 0, 0),
-                        0,
-                        24,
-                        "마스크 꼭 착용하세요"
-                ), EventResponse.of(
-                        2L,
-                        1L,
-                        "오후 운동",
-                        EventStatus.OPENED,
-                        LocalDateTime.of(2022, 8, 31, 9, 0, 0),
-                        LocalDateTime.of(2022, 8, 31, 18, 0, 0),
-                        0,
-                        24,
-                        "마스크 꼭 착용하세요"
+        List<EventResponse> events = eventService.getEvents(predicate)
+                .stream()
+                .map(EventResponse::from)
+                .toList();
 
-                )
-        ));
+        map.put("events", events);
 
         return new ModelAndView("event/index", map);
     }
@@ -51,17 +41,11 @@ public class EventController {
     @GetMapping("/{eventId}")
     public ModelAndView eventDetail(@PathVariable Long eventId) {
         Map<String, Object> map = new HashMap<>();
-        map.put("event", EventResponse.of(
-                eventId,
-                1L,
-                "오후 운동",
-                EventStatus.OPENED,
-                LocalDateTime.of(2022, 8, 31, 9, 0, 0),
-                LocalDateTime.of(2022, 8, 31, 18, 0, 0),
-                0,
-                24,
-                "마스크 꼭 착용하세요"
-        ));
+        EventResponse event = eventService.getEvent(eventId)
+                .map(EventResponse::from)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
+
+        map.put("event", event);
 
         return new ModelAndView("event/detail", map);
     }
